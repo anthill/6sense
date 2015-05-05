93eabd8073b5b5c858dc2a90f13472d9835d1163
Alexandre Vallette <vallettea@gmail.com>
2015-05-05 09:09:04 +0200

    added data and change the path of output file

--- shell/runMonitoring.sh
+++ shell/runMonitoring.sh
@@ -2,4 +2,4 @@
 sudo airmon-ng start wlan0 
  
 # listen
-sudo airodump-ng --output-format csv --berlin 300 --write /home/pi/6sense/data/report --write-interval 300 wlan0mon
+sudo airodump-ng --output-format csv --berlin 300 --write data/report --write-interval 300 wlan0mon
